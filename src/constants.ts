eeb1961d0d88df74305818e68079c29a3ad173f2
tgcooldev <76828588+tgcooldev@users.noreply.github.com>
2021-07-11 04:17:09 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x566b73A916c198A338c3b1e8d15E82dB8120E435'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
-export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(100)
 
 // exports for internal consumption
 export const ZERO = JSBI.BigInt(0)
@@ -12,3 +12,4 @@ export const ONE = JSBI.BigInt(1)
 export const FIVE = JSBI.BigInt(5)
 export const _997 = JSBI.BigInt(997)
 export const _1000 = JSBI.BigInt(1000)
+export const _59 = JSBI.BigInt(59)
